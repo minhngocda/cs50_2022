c744300d675ad9de74068261ac05ae52e0bb0b2a
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-02 20:58:23 +0000

    Sun, Jan 2, 2022, 8:58 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,9 +1,10 @@
 x = int(input('Height: '))
-    while (0 < x and x < 9):
-        break
-for i in range (x):
-    for k in range (x - 1):
-        print (" ")
-    for l in range (x):
-        print ("#")
-    print ("\n")
+if x <= 0 and x > 9:
+    x = int(input('Height: '))
+else:
+    for i in range (x):
+        for k in range (x - 1):
+            print (" ")
+        for l in range (x):
+            print ("#")
+        print ("\n")

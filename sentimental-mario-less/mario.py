0db951b5574aafd87609bb3d582893db0a0ebedf
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-02 20:49:58 +0000

    Sun, Jan 2, 2022, 8:49 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,5 +1,10 @@
 while True:
         x = input('Height: ')
-        if x.isdigit():
+        if x.is():
             break
-for 
+for i in range (x):
+    for k in range (x - 1):
+        print (" ")
+    for l in range (x):
+        print ("#")
+    print ("\n")

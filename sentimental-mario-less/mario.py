e9c1a3600bb525450c13f2335b34f84558247fd1
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-03 10:42:43 +0000

    Mon, Jan 3, 2022, 10:42 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,12 +1,13 @@
 def main():
     height = get_height()
     for i in range(height):
+        
         print("#")
 
 def get_height():
     while True:
         n = int(input("Height: "))
-        if n > 0:
+        if n > 0 and n <9:
             break
     return n
 main()

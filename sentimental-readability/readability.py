c15145b83b2944e4c787df2c13007748f65c475b
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-04 15:49:49 +0000

    Tue, Jan 4, 2022, 3:49 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,12 +1,17 @@
 from cs50 import get_string
 
 def main():
+
+def count_letter():
     x = get_input()
     count_letter = 0
     for i in x:
         if i.isalpha():
             count_letter += 1
-    print(count_letter)
+    return count_letter
+
+def count_words():
+    
 
 
 def get_input():

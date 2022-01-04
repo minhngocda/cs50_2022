9481788a7e62ce78b94d1b076b3c2e67bd98c3dd
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-04 16:31:12 +0000

    Tue, Jan 4, 2022, 4:31 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,6 +3,10 @@ from cs50 import get_string
 def main():
     x = count_letter(get_input())
     L = count_words(get_input())
+    S = count_sentence(get_input())
+    print( x + ' letters')
+    print( L + ' words')
+    print( S + ' sentences')
 
 def count_letter(x):
     #x = get_input()
@@ -16,17 +20,21 @@ def count_words(x):
     word = 0
     count_space = 0
     for i in x:
-        if i = " ":
+        if i == " ":
             count_space += 1
-    if count_space =! 0:
+    if count_space != 0:
         word = count_space + 1
     else:
         if count_letter(x) > 0:
             word = 1
     return word
 
-
-
+def count_sentence(x):
+    sentence = 0
+    for i in x:
+        if i.isalpha() and (i+1 == '.' or i+1 == '!' or i+1 == '?'):
+            sentence += 1
+    return sentence
 
 
 def get_input():

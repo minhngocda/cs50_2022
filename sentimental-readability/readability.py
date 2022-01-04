a030096469bd1a9e7a5b46dd25074b06d21b5c17
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-04 15:27:46 +0000

    Tue, Jan 4, 2022, 3:27 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,11 +1,15 @@
 from cs50 import get_string
 
 def main():
-    text = get_input
-    x = count(text)
-    print (x)
+    x = get_input
+    count_letter = 0
+    for i in x:
+        if i =
+        
+
 
 def get_input():
     answer = get_string("Text: ")
     return answer
+
 main()

72e6afebe6eec87e3b70f891eb2ebcaf333e5d8e
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-04 09:00:39 +0000

    Tue, Jan 4, 2022, 9:00 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,10 +1,10 @@
 from cs50 import get_float
-def get_input()
+def get_input():
     while True:
         try:
             n = float("Change owed: ")
             if n > 0:
                 break
         except ValueError:
-        print("That's not desired value!")
+            print("That's not desired value!")
 get_input()

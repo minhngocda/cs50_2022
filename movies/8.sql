d4c58bcdd89318b4ca5e634c643a47d88656f850
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-20 21:17:08 +0000

    Thu, Jan 20, 2022, 9:17 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,4 +2,4 @@
 --Your query should output a table with a single column for the name of each person.
 --You may assume that there is only one movie in the database with the title Toy Story.
 SELECT name FROM people
-WHERE person_id = (SELECT PRIMARY KEY(id) WHERE title = 'Toy Story');
+WHERE movie_id FROM stars = (SELECT id FROM movies WHERE title = 'Toy Story');

43213e1fc6abe320278e182cd4fe38d348d5ba57
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-20 09:50:18 +0000

    Thu, Jan 20, 2022, 9:50 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -2,3 +2,4 @@
 --Your query should output a table with two columns, one for the title of each movie and one for the rating of each movie.
 --Movies that do not have ratings should not be included in the result.
 SELECT title FROM movies AND rating FROM ratings
+    WHERE ....;

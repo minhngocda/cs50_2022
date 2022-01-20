70f9b4fc9101c4dd98e9a2d513f3326b53979853
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-20 09:54:22 +0000

    Thu, Jan 20, 2022, 9:54 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -2,5 +2,4 @@
 --Your query should output a table with two columns, one for the title of each movie and one for the rating of each movie.
 --Movies that do not have ratings should not be included in the result.
 SELECT title FROM movies WHERE year = 2010
-AND SELECT rating FROM ratings ORDER BY rating
-    WHERE ....;
+SELECT rating FROM ratings ORDER BY rating;

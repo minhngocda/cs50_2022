a256d79629e1b8253220e1460b91ef8805637bfa
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-20 09:31:43 +0000

    Thu, Jan 20, 2022, 9:31 AM +00:00

--- movies/5.sql
+++ movies/5.sql
@@ -2,6 +2,6 @@
 --Your query should output a table with two columns, one for the title of each movie and one for the release year of each movie.
 --You may assume that the title of all Harry Potter movies will begin with the words “Harry Potter”, and that if a movie title begins with the words “Harry Potter”, it is a Harry Potter movie.
 CREATE TABLE HARRY POTTER AS
-    SELECT column1, column2,...
+    SELECT title, column2,...
     FROM existing_table_name
     WHERE ....;

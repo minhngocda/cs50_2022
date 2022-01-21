77fd9db56a61ea160e3d9a5dc34bd782c3c82fe0
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-21 18:05:02 +0000

    Fri, Jan 21, 2022, 6:05 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -6,10 +6,9 @@
 SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON movies.id = stars.movie_id
-where movies.id in
-(select movies.id from movies
-join people on stars.person_id =people.id
-join stars on stars.movie_id=movies.id
-where people.name="Kevin Bacon"
-and people.birth=1958)
-and people.name != "Kevin Bacon";
+WHERE movies.id in
+(SELECT movies.id FROM movies
+JOIN people ON stars.person_id = people.id
+JOIN stars ON stars.movie_id = movies.id
+WHERE people.name="Kevin Bacon" AND people.birth = 1958)
+AND people.name != "Kevin Bacon";

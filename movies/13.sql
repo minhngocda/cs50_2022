e05aabb23179ca18c21c493674eabe22491a554c
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-21 18:02:01 +0000

    Fri, Jan 21, 2022, 6:02 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,3 +2,7 @@
 --Your query should output a table with a single column for the name of each person.
 --There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
 --Kevin Bacon himself should not be included in the resulting list.
+
+SELECT DISTINCT name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON movies.id = stars.movie_id

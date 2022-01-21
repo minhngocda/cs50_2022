e5e3d2f65b34de7ef652ccbaa4d3f3600f0b1c39
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-21 17:53:36 +0000

    Fri, Jan 21, 2022, 5:53 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,14 +2,9 @@
 --Your query should output a table with a single column for the title of each movie.
 --You may assume that there is only one person in the database with the name Chadwick Boseman.
 
---SELECT title FROM movies
---JOIN stars ON stars.movie_id = movies.id
---JOIN people ON people.id = stars.person_id WHERE name = "Chadwick Boseman"
---JOIN ratings ON ratings.movie_id = movies.id ORDER BY ratings.rating DESC LIMIT 5;
-select movies.title from movies
-join stars on stars.movie_id=movies.id
-join people on stars.person_id=people.id
-join ratings on ratings.movie_id=movies.id
-where people.name="Chadwick Boseman"
-order by ratings.rating desc
-limit 5;
+SELECT title FROM movies
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON people.id = stars.person_id
+JOIN ratings ON ratings.movie_id = movies.id
+WHERE people.name = "Chadwick Boseman"
+ORDER BY ratings.rating DESC LIMIT 5;

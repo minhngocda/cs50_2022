bc01953e008bd7d6182123a69d3c4fa9dc48a789
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-23 10:24:14 +0000

    Sun, Jan 23, 2022, 10:24 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,3 +2,6 @@
 -- The theft took place on July 28, 2021 and that it took place on Humphrey Street.
 select description from crime_scene_reports
 where month = 7 and day = 28 and year = 2021 and street = 'Humphrey Street';
+
+select activity from bakery_security_logs
+where month = 7 and day = 28 and year = 2021 and hour = 10 and minute = 15;

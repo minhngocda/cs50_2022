2ee0af31b5a6639cb89e35ba473d6d59207b9592
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-23 15:41:13 +0000

    Sun, Jan 23, 2022, 3:41 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,11 +5,6 @@ where month = 7 and day = 28 and year = 2021 and street = 'Humphrey Street';
 --Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
 -- Littering took place at 16:36. No known witnesses.
 
--- show licencse plate of car which at 10 a.m in/out bakery
-select license_plate from bakery_security_logs
---join bakery_security_logs on people.license_plate = bakery_security_logs.license_plate
-where bakery_security_logs.month = 7 and bakery_security_logs.day = 28
-and bakery_security_logs.year = 2021 and bakery_security_logs.hour = 10;
 
 -- show people whose car at 10 a.m in/out bakery
 select passport_number,name from people

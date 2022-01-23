d2fae23eaa44e0b4f110fac8b286657c025c4494
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-23 10:53:13 +0000

    Sun, Jan 23, 2022, 10:53 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,7 +3,7 @@
 select description from crime_scene_reports
 where month = 7 and day = 28 and year = 2021 and street = 'Humphrey Street';
 --Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
-|-- Littering took place at 16:36. No known witnesses.
+-- Littering took place at 16:36. No known witnesses.
 
 
 select license_plate from bakery_security_logs

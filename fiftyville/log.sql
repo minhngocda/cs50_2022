5b68fd80f520d3dc49eb8bd5db28e5113aa9cee9
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-23 10:08:23 +0000

    Sun, Jan 23, 2022, 10:08 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+-- The theft took place on July 28, 2021 and that it took place on Humphrey Street.
+select description from crime_scene_reports
+where month = 7 and day = 28 and year = 2021 and street = 'Humphrey Street';

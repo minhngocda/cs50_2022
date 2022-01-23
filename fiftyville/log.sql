eaba4a136821979c6fd5cf27245e0f06bbdfef2c
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-23 15:44:36 +0000

    Sun, Jan 23, 2022, 3:44 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,9 +8,13 @@ where month = 7 and day = 28 and year = 2021 and street = 'Humphrey Street';
 
 -- show people whose car at 10 a.m in/out bakery
 select passport_number,name from people
-join bakery_security_logs on people.license_plate = bakery_security_logs.license_plate
+inner join bakery_security_logs on people.license_plate = bakery_security_logs.license_plate
 where bakery_security_logs.month = 7 and bakery_security_logs.day = 28
-and bakery_security_logs.year = 2021 and bakery_security_logs.hour = 10;
+and bakery_security_logs.year = 2021 and bakery_security_logs.hour = 10
+where id = (select person_id from bank_accounts
+join atm_transactions on bank_accounts.account_number = atm_transactions.account_number
+where atm_transactions.month = 7 and atm_transactions.day = 28 and atm_transactions.year = 2021
+and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transaction_type = 'withdraw');
 
 --I don't know the thief's name, but it was someone I recognized. Earlier this morning,
 --before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.

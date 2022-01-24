b052bad70b23727be1c9463b7ed2bf1d1c60be88
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-24 09:37:17 +0000

    Mon, Jan 24, 2022, 9:37 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -34,12 +34,10 @@ where phone_calls.year = 2021 and phone_calls.month = 7 and phone_calls.day = 28
 group by(phone_calls.caller)
 having count(phone_calls.caller) >=2;
 
-
 join passengers on BANK.passport_number = passengers.passport_number
 join flights on flights.id = passengers.flight_id
 join airports on flights.origin_airport_id = airports.id
 where airports.city = 'Fiftyville' and flights.year = 2021 and flights.month = 7 and flights.day = 29
-
 
 SELECT BANK.passport_number , BANK.name , BANK.id, flights.id FROM BANK
 join passengers on BANK.passport_number = passengers.passport_number

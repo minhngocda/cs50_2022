6762129b6dd333263edc2859183e7117e8bf790b
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-24 09:41:33 +0000

    Mon, Jan 24, 2022, 9:41 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,23 +6,23 @@ where month = 7 and day = 28 and year = 2021 and street = 'Humphrey Street';
 --Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
 -- Littering took place at 16:36. No known witnesses.
 -- show people whose car at 10 a.m in/out bakery
-CREATE TABLE CAR AS
-    select people.passport_number, people.name, people.id from people
-    join bakery_security_logs on people.license_plate = bakery_security_logs.license_plate
-    where bakery_security_logs.month = 7 and bakery_security_logs.day = 28
-    and bakery_security_logs.year = 2021 and bakery_security_logs.hour = 10
-    order by people.name;
+--CREATE TABLE CAR AS
+  --  select people.passport_number, people.name, people.id from people
+    --join bakery_security_logs on people.license_plate = bakery_security_logs.license_plate
+  --  where bakery_security_logs.month = 7 and bakery_security_logs.day = 28
+  --  and bakery_security_logs.year = 2021 and bakery_security_logs.hour = 10
+  --  order by people.name;
 
 --I don't know the thief's name, but it was someone I recognized. Earlier this morning,
 --before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
 -- this show personal_id of people who withdraw money on that day
-CREATE TABLE BANK AS
-    select people.passport_number, people.name, people.id, people.phone_number from people
-    join bank_accounts on bank_accounts.person_id = people.id
-    join atm_transactions on bank_accounts.account_number = atm_transactions.account_number
-    where atm_transactions.month = 7 and atm_transactions.day = 28 and atm_transactions.year = 2021
-    and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transaction_type = 'withdraw'
-    order by people.name;
+--CREATE TABLE BANK AS
+  --  select people.passport_number, people.name, people.id, people.phone_number from people
+  --  join bank_accounts on bank_accounts.person_id = people.id
+  --  join atm_transactions on bank_accounts.account_number = atm_transactions.account_number
+  --  where atm_transactions.month = 7 and atm_transactions.day = 28 and atm_transactions.year = 2021
+  --  and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transaction_type = 'withdraw'
+  --  order by people.name;
 
  --I'm the bakery owner, and someone came in, suspiciously whispering into a phone for about half an hour
 --As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
@@ -34,10 +34,10 @@ where phone_calls.year = 2021 and phone_calls.month = 7 and phone_calls.day = 28
 group by(phone_calls.caller)
 having count(phone_calls.caller) >=2;
 
-join passengers on BANK.passport_number = passengers.passport_number
-join flights on flights.id = passengers.flight_id
-join airports on flights.origin_airport_id = airports.id
-where airports.city = 'Fiftyville' and flights.year = 2021 and flights.month = 7 and flights.day = 29
+--join passengers on BANK.passport_number = passengers.passport_number
+--join flights on flights.id = passengers.flight_id
+--join airports on flights.origin_airport_id = airports.id
+--where airports.city = 'Fiftyville' and flights.year = 2021 and flights.month = 7 and flights.day = 29
 
 SELECT BANK.passport_number , BANK.name , BANK.id, flights.id FROM BANK
 join passengers on BANK.passport_number = passengers.passport_number

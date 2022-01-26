02cf81c1995d0d2004b34362e8c499c26dc529df
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 09:27:21 +0000

    Wed, Jan 26, 2022, 9:27 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,6 @@
 SELECT name FROM people
-WHERE people.license_plate
+-- Using bakery_security_logs to find out license plate out of bakery on that time of theft
+WHERE people.license_plate IN
+(SELECT license_plate FROM bakery_security_logs
+WHERE bakery_security_logs.year=2021 and bakery_security_logs.month=7 and bakery_security_logs.day=28
+and bakery_security_logs.hour=10)

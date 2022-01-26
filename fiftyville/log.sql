19e0114707fd2105d17198ad9e1c1306855af327
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 14:30:53 +0000

    Wed, Jan 26, 2022, 2:30 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,16 +4,19 @@ WHERE people.license_plate IN
 (SELECT license_plate FROM bakery_security_logs
 WHERE bakery_security_logs.year=2021 and bakery_security_logs.month=7 and bakery_security_logs.day=28
 and bakery_security_logs.hour=10 and bakery_security_logs.minute > 15 and bakery_security_logs.minute<30)
+
 -- using phone calls to find our phone number that have a call on that day less thang 1 minute
 AND people.phone_number IN
 (SELECT caller FROM phone_calls
 WHERE phone_calls.year=2021 and phone_calls.month=7 and phone_calls.day=28 and phone_calls.duration<60)
+
 --using atm transaction, bank accounts to find id of people who withdraw money from ATM on Legget Street
 AND people.id IN
 (SELECT person_id FROM bank_accounts
 JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
 WHERE atm_transactions.year=2021 and atm_transactions.month=7 and atm_transactions.day=28
 and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transaction_type = 'withdraw')
+
 -- Using passenger, flight_id,  and airport to find all passenger on the earliest flight from Fiftyville on the next day of the theft
 AND people.passport_number IN
 (SELECT passport_number FROM passengers
@@ -22,6 +25,7 @@ WHERE flight_id IN
 and flights.origin_airport_id in (SELECT id FROM airports WHERE airports.city = 'Fiftyville')
 ORDER BY flights.hour, flights.minute ASc limit 1) );
 
+--using airports, flights to find down where the earliest flight on the next day heading to
 SELECT city FROM airports
 WHERE id IN
 (SELECT destination_airport_id FROM flights WHERE flights.year = 2021 and flights.month = 7 and flights.day = 29

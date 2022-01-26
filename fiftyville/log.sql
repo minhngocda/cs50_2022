971a7bdb96b37e40d8b8f885dff91bc5d29c63b9
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 09:56:33 +0000

    Wed, Jan 26, 2022, 9:56 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,9 +9,5 @@ where people.phone_number IN
 (SELECT caller FROM phone_calls
 WHERE phone_calls.year=2021 and phone_calls.month=7 and phone_calls.day=28 and phone_calls.duration<60);
 --using atm transaction, bank accounts to find id of people who withdraw money from ATM on Legget Street
-where people.id IN
-(SELECT person_id FROM bank_accounts
-JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
-WHERE atm_transactions.year=2021 and atm_transactions.month=7 and atm_transactions.day=28
-and atm_transactions.atm_location = 'Legget Street' and atm_transactions.transaction_type = 'withdraw');
+where people.id = BANK.id;
 ;

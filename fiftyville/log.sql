8c0c609530452eaeb049dd41e0ffcbdae4e788fb
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 09:44:37 +0000

    Wed, Jan 26, 2022, 9:44 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,4 +3,11 @@ SELECT name FROM people
 WHERE people.license_plate IN
 (SELECT license_plate FROM bakery_security_logs
 WHERE bakery_security_logs.year=2021 and bakery_security_logs.month=7 and bakery_security_logs.day=28
-and bakery_security_logs.hour=10)
+and bakery_security_logs.hour=10 and bakery_security_logs.minute > 20)
+-- using phone calls to find our phone number that have a call on that day less thang 1 minute
+AND phone_number IN
+(SELECT caller FROM phone_calls
+WHERE phone_calls.year=2021 and phone_calls.month=7 and phone_calls.day=28 and phone_calls.duration<60)
+--using atm transaction, bank accounts to find id of people who withdraw money from ATM on Legget Street
+AND people.id IN
+(SELECT )

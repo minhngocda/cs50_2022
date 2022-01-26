5af78a5ba9d415a96274bf679af3438b314251a4
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 09:52:38 +0000

    Wed, Jan 26, 2022, 9:52 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,15 +3,15 @@ SELECT name FROM people
 WHERE people.license_plate IN
 (SELECT license_plate FROM bakery_security_logs
 WHERE bakery_security_logs.year=2021 and bakery_security_logs.month=7 and bakery_security_logs.day=28
-and bakery_security_logs.hour=10 and bakery_security_logs.minute > 20)
+and bakery_security_logs.hour=10 and bakery_security_logs.minute > 20);
 -- using phone calls to find our phone number that have a call on that day less thang 1 minute
-AND phone_number IN
+where people.phone_number IN
 (SELECT caller FROM phone_calls
-WHERE phone_calls.year=2021 and phone_calls.month=7 and phone_calls.day=28 and phone_calls.duration<60)
+WHERE phone_calls.year=2021 and phone_calls.month=7 and phone_calls.day=28 and phone_calls.duration<60);
 --using atm transaction, bank accounts to find id of people who withdraw money from ATM on Legget Street
-AND people.id IN
+where people.id IN
 (SELECT person_id FROM bank_accounts
 JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
 WHERE atm_transactions.year=2021 and atm_transactions.month=7 and atm_transactions.day=28
-and atm_transactions.atm_location = 'Legget Street' and atm_transactions.transaction_type = 'withdraw')
+and atm_transactions.atm_location = 'Legget Street' and atm_transactions.transaction_type = 'withdraw');
 ;

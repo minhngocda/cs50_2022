61855d3c36ad9937b2ef5cc3c43605a92756b2fe
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 10:03:25 +0000

    Wed, Jan 26, 2022, 10:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -14,3 +14,12 @@ AND people.id IN
 JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
 WHERE atm_transactions.year=2021 and atm_transactions.month=7 and atm_transactions.day=28
 and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transaction_type = 'withdraw');
+AND people.passport_number IN
+(SELECT passport_number FROM passengers
+JOIN flights ON flights.id = passengers.flight_id
+WHERE )
+
+
+
+
+

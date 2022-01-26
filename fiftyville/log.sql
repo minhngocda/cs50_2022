d7e16bd9fc83cb787e8b1b8fbe60405912f457d8
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 09:59:08 +0000

    Wed, Jan 26, 2022, 9:59 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,5 +13,5 @@ where people.id IN
 (SELECT person_id FROM bank_accounts
 JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
 WHERE atm_transactions.year=2021 and atm_transactions.month=7 and atm_transactions.day=28
-and atm_transactions.atm_location = 'Legget Street' and atm_transactions.transaction_type = 'withdraw');
+and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transaction_type = 'withdraw');
 ;

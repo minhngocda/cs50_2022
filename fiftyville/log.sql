368599520cae2cd0dd1d10b21948861deb5d5362
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-01-26 10:16:50 +0000

    Wed, Jan 26, 2022, 10:16 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -17,10 +17,9 @@ and atm_transactions.atm_location = 'Leggett Street' and atm_transactions.transa
 WHERE people.passport_number IN
 (SELECT passport_number FROM passengers
 JOIN flights ON flights.id = passengers.flight_id
-WHERE flights.year = 2021 and flights.month = 7 and flights.day = 29 ORDER BY flights.hour and flights.minute ASC LIMIT 1 
-and flights.origin_airport_id in (SELECT id FROM airports WHERE airports.city = 'Fiftyville'
-))
-
+WHERE flights.year = 2021 and flights.month = 7 and flights.day = 29
+and flights.origin_airport_id in (SELECT id FROM airports WHERE airports.city = 'Fiftyville')
+ORDER BY flights.hour and flights.minute ASc);
 
 
 

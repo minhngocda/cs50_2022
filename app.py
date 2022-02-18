4d5268a9511cbd5bd24b5cdcda18be2e71dd1b6b
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-02-18 12:49:06 +0000

    Fri, Feb 18, 2022, 12:49 PM +00:00

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     name = request.args.get("name")
-    return render_template("index.html", name=name)
+    return render_template("index.html")
+
+
+@app.route("/greet")
+def greet():
+    name = request.args.get("name")
+    return render_template("greet.html",name=name)

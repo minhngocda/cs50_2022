8ba90a4034f7c0ea8c0fa7adeaef839e134101e4
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-02-18 17:32:05 +0000

    Fri, Feb 18, 2022, 5:32 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -34,10 +34,10 @@ def index():
         day = request.form.get("day")
 
         #Insert data into database
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES('Ngoc', 2, 21)")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
     else:
-        #birthdays = db.execute("SELECT * FROM birthdays;")
+        birthdays = db.execute("SELECT * FROM birthdays;")
         return render_template("index.html")

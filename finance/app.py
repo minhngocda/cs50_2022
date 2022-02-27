56445d3c5aa7b7e55a933be6e16fe5c3d5a95479
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-02-27 18:52:38 +0000

    Sun, Feb 27, 2022, 6:52 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -121,7 +121,7 @@ def quote():
         result = lookup(symbol)
         if not result:
             return apology("invalid symbol", 400)
-        return render_template("quoted.html", name = result["name"], price = usd(result["price"]), symbol = result["symbol"])
+        return render_template("quoted.html", result)
 
     else:
         return render_template("quote.html")

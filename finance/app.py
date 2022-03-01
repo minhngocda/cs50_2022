9e866f237f74dfb2d85cfbd797715ba81c50e685
Jenny Nguyen <68969499+minhngocdev@users.noreply.github.com>
2022-03-01 09:12:50 +0000

    Tue, Mar 1, 2022, 9:12 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -51,8 +51,8 @@ def own_shares():
     # filter zero-share stocks
     owns = {k: v for k, v in owns.items() if v != 0}
     return owns
-    
-@app.route("/")
+
+@app.route("/"
 @login_required
 def index():
     """Show portfolio of stocks"""
